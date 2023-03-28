f4d1a65c61fc4f58f3f97bfdc1ab7c294ec15593
ihsancelik <a.ihsan.celik96@gmail.com>
2023-03-28 11:38:54 +0000

    before/after create, update, destroy, save

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,32 @@
 class Category < ApplicationRecord
+
+    after_save :after_saved
+    before_save :before_saved
+    before_destroy :before_destroyed
+    before_update :before_updated
+    after_create :send_notification
+
+
+    private
+    
+    def after_saved
+        print "\n@@@@@ after saved run! @@@@\n"
+    end
+
+    def before_saved
+        print "\n@@@@@ before saved run! @@@@\n"
+    end
+
+    def before_destroyed
+        print "\n@@@@@ before destroyed run! @@@@\n"
+    end
+
+    def before_updated
+        print "\n@@@@@ before updated run! @@@@\n"
+    end
+
+    def send_notification
+        print "Notification sent!"
+    end
+
 end
